5f6e845872a060968f5dcc741ad103d819bca0fb
renaissanceee <st179481@stud.uni-stuttgart.de>
2024-07-18 09:11:28 +0200

    deform

---
+++ deform/deform_model.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+from scene import DeformNetwork
+from scene.utils import searchForMaxIteratio,get_expon_lr_func
+
+
+class DeformModel:
+    def __init__(self, is_blender=True, is_6dof=False):
+        self.deform = DeformNetwork(is_blender=is_blender, is_6dof=is_6dof).cuda()
+        self.optimizer = None
+        self.spatial_lr_scale = 5
+        self.position_lr_init = 0.00016
+        self.position_lr_final = 0.0000016
+        self.position_lr_delay_mult = 0.01
+        self.position_lr_max_steps = 30_000
+        self.deform_lr_max_steps = 40_000
+
+        if model_path is not None:
+            print(f"loading model path:{model_path}")
+            checkpoint = torch.load(model_path, map_location=self.device)
+            model_dict = self.gaussian_model.state_dict()
+            pretrained_dict = {k: v for k, v in checkpoint.items() if k in model_dict}
+            model_dict.update(pretrained_dict)
+            self.gaussian_model.load_state_dict(model_dict)
+
+    def step(self, xyz, time_emb):
+        return self.deform(xyz, time_emb)
+
+    def train_setting(self):
+        l = [
+            {'params': list(self.deform.parameters()),
+             'lr': training_args.position_lr_init * self.spatial_lr_scale,
+             "name": "deform"}
+        ]
+        self.optimizer = torch.optim.Adam(l, lr=0.0, eps=1e-15)
+        self.deform_scheduler_args = get_expon_lr_func(lr_init=self.position_lr_init * self.spatial_lr_scale,
+                                                       lr_final=self.position_lr_final,
+                                                       lr_delay_mult=tself.position_lr_delay_mult,
+                                                       max_steps=self.deform_lr_max_steps)
+
+    def save_weights(self, model_path, iteration):
+        out_weights_path = os.path.join(model_path, "deform/iteration_{}".format(iteration))
+        os.makedirs(out_weights_path, exist_ok=True)
+        torch.save(self.deform.state_dict(), os.path.join(out_weights_path, 'deform.pth'))
+
+    def load_weights(self, model_path, iteration=-1):
+        if iteration == -1:
+            loaded_iter = searchForMaxIteration(os.path.join(model_path, "deform"))
+        else:
+            loaded_iter = iteration
+        weights_path = os.path.join(model_path, "deform/iteration_{}/deform.pth".format(loaded_iter))
+        self.deform.load_state_dict(torch.load(weights_path))
+
+    def update_learning_rate(self, iteration):
+        for param_group in self.optimizer.param_groups:
+            if param_group["name"] == "deform":
+                lr = self.deform_scheduler_args(iteration)
+                param_group['lr'] = lr
+                return lr
--- train_video.py
+++ train_video.py
@@ -13,19 +13,20 @@ from utils import *
 from tqdm import tqdm
 import random
 import torchvision.transforms as transforms
+from train import SimpleTrainer2d
+from random import randint
+import re
 
-
-class SimpleTrainer2d:
-    """Trains random 2d gaussians to fit an image."""
-
+class DefTrainer2d:
+    """Train 2d gaussians to fit frame_i."""
     def __init__(
-            self,
-            image_path: Path,
-            num_points: int = 2000,
-            model_name: str = "GaussianImage_Cholesky",
-            iterations: int = 30000,
-            model_path=None,
-            args=None,
+        self,
+        image_path: Path,
+        num_points: int = 2000,
+        model_name:str = "GaussianImage_Cholesky",
+        iterations:int = 100,
+        model_path = None,
+        args = None,
     ):
         self.device = torch.device("cuda:0")
         self.gt_image = image_path_to_tensor(image_path).to(self.device)
@@ -37,27 +38,31 @@ class SimpleTrainer2d:
         self.H, self.W = self.gt_image.shape[2], self.gt_image.shape[3]
         self.iterations = iterations
         self.save_imgs = args.save_imgs
-        self.log_dir = Path(
-            f"./checkpoints/{args.data_name}/{model_name}_{args.iterations}_{num_points}/{self.image_name}")
-
+        self.log_dir = Path(f"./checkpoints/{args.data_name}/{model_name}_{args.iterations}_{num_points}/{self.image_name}")
+        # match = re.search(r'\d+', self.image_name);self.t = int(match.group())
+        image_name = self.image_name.split(".")[0]# r_23
+        self.t = image_name.split("_")[1]# 23
+        fid = int(self.t)/199
+        # 2DGS
         if model_name == "GaussianImage_Cholesky":
-            from gaussianimage_cholesky import GaussianImage_Cholesky
-            self.gaussian_model = GaussianImage_Cholesky(loss_type="L2", opt_type="adan", num_points=self.num_points,
-                                                         H=self.H, W=self.W, BLOCK_H=BLOCK_H, BLOCK_W=BLOCK_W,
-                                                         device=self.device, lr=args.lr, quantize=False).to(self.device)
+            from gaussianvideo_cholesky import GaussianVideo_Cholesky
+            self.gaussian_model = GaussianVideo_Cholesky(t=self.t, loss_type="L2", opt_type="adan", num_points=self.num_points, H=self.H, W=self.W, BLOCK_H=BLOCK_H, BLOCK_W=BLOCK_W, 
+                device=self.device, lr=args.lr, quantize=False).to(self.device)
 
         elif model_name == "GaussianImage_RS":
             from gaussianimage_rs import GaussianImage_RS
-            self.gaussian_model = GaussianImage_RS(loss_type="L2", opt_type="adan", num_points=self.num_points,
-                                                   H=self.H, W=self.W, BLOCK_H=BLOCK_H, BLOCK_W=BLOCK_W,
-                                                   device=self.device, lr=args.lr, quantize=False).to(self.device)
+            self.gaussian_model = GaussianImage_RS(loss_type="L2", opt_type="adan", num_points=self.num_points, H=self.H, W=self.W, BLOCK_H=BLOCK_H, BLOCK_W=BLOCK_W, 
+                device=self.device, lr=args.lr, quantize=False).to(self.device) 
 
         elif model_name == "3DGS":
             from gaussiansplatting_3d import Gaussian3D
-            self.gaussian_model = Gaussian3D(loss_type="Fusion2", opt_type="adan", num_points=self.num_points, H=self.H,
-                                             W=self.W, BLOCK_H=BLOCK_H, BLOCK_W=BLOCK_W,
-                                             device=self.device, sh_degree=args.sh_degree, lr=args.lr).to(self.device)
-
+            self.gaussian_model = Gaussian3D(loss_type="Fusion2", opt_type="adan", num_points=self.num_points, H=self.H, W=self.W, BLOCK_H=BLOCK_H, BLOCK_W=BLOCK_W, 
+                device=self.device, sh_degree=args.sh_degree, lr=args.lr).to(self.device)
+        # deform-NN
+        # 类比gaussian_renderer, 获取偏移量
+        self.deform_model = DeformModel()
+        self.deform_model.train_setting()
+        
         self.logwriter = LogWriter(self.log_dir)
 
         if model_path is not None:
@@ -67,20 +72,31 @@ class SimpleTrainer2d:
             pretrained_dict = {k: v for k, v in checkpoint.items() if k in model_dict}
             model_dict.update(pretrained_dict)
             self.gaussian_model.load_state_dict(model_dict)
+            self.deform_model.load_weights(model_path, iteration=-1)
 
-    def train(self):
+    def train(self):     
         psnr_list, iter_list = [], []
-        progress_bar = tqdm(range(1, self.iterations + 1), desc="Training progress")
+        progress_bar = tqdm(range(1, self.iterations+1), desc="Training progress")
         best_psnr = 0
         self.gaussian_model.train()
         start_time = time.time()
-        for iter in range(1, self.iterations + 1):
-            loss, psnr = self.gaussian_model.train_iter(self.gt_image)
+        for iter in range(1, self.iterations+1):
+            # ---------------------------------------
+            if iter < 10_000:  # warm_up = 10_000
+                d_xyz, d_cholesky = 0.0, 0.0, 0.0
+            else:
+                time_input = self.fid.unsqueeze(0).expand(self.gaussian_model.get_xyz.shape[0], -1)
+                d_xyz, d_cholesky = self.deform_model.step(self.gaussian_model.get_xyz.detach(), time_input)
+                # ast_noise = 0 if dataset.is_blender else torch.randn(1, 1, device='cuda').expand(N,-1) * time_interval * smooth_term(iteration)
+                # d_xyz, d_rotation, d_scaling = deform.step(self.gaussian_model.get_xyz.detach(), time_input + ast_noise)
+            # ---------------------------------------
+            # loss, psnr = self.gaussian_model.train_iter(self.gt_image, iter)
+            loss, psnr = self.gaussian_model.train_iter(self.gt_image, iter, d_xyz, d_cholesky)
             psnr_list.append(psnr)
             iter_list.append(iter)
             with torch.no_grad():
                 if iter % 10 == 0:
-                    progress_bar.set_postfix({f"Loss": f"{loss.item():.{7}f}", "PSNR": f"{psnr:.{4}f},"})
+                    progress_bar.set_postfix({f"Loss":f"{loss.item():.{7}f}", "PSNR":f"{psnr:.{4}f},"})
                     progress_bar.update(10)
         end_time = time.time() - start_time
         progress_bar.close()
@@ -90,17 +106,14 @@ class SimpleTrainer2d:
             test_start_time = time.time()
             for i in range(100):
                 _ = self.gaussian_model()
-            test_end_time = (time.time() - test_start_time) / 100
+            test_end_time = (time.time() - test_start_time)/100
 
-        self.logwriter.write(
-            "Training Complete in {:.4f}s, Eval time:{:.8f}s, FPS:{:.4f}".format(end_time, test_end_time,
-                                                                                 1 / test_end_time))
+        self.logwriter.write("Training Complete in {:.4f}s, Eval time:{:.8f}s, FPS:{:.4f}".format(end_time, test_end_time, 1/test_end_time))
         torch.save(self.gaussian_model.state_dict(), self.log_dir / "gaussian_model.pth.tar")
-        np.save(self.log_dir / "training.npy",
-                {"iterations": iter_list, "training_psnr": psnr_list, "training_time": end_time,
-                 "psnr": psnr_value, "ms-ssim": ms_ssim_value, "rendering_time": test_end_time,
-                 "rendering_fps": 1 / test_end_time})
-        return psnr_value, ms_ssim_value, end_time, test_end_time, 1 / test_end_time
+        self.deform.save_weights(model_path, self.iterations)
+        np.save(self.log_dir / "training.npy", {"iterations": iter_list, "training_psnr": psnr_list, "training_time": end_time, 
+        "psnr": psnr_value, "ms-ssim": ms_ssim_value, "rendering_time": test_end_time, "rendering_fps": 1/test_end_time})
+        return psnr_value, ms_ssim_value, end_time, test_end_time, 1/test_end_time
 
     def test(self):
         self.gaussian_model.eval()
@@ -113,17 +126,15 @@ class SimpleTrainer2d:
         if self.save_imgs:
             transform = transforms.ToPILImage()
             img = transform(out["render"].float().squeeze(0))
-            name = self.image_name + "_fitting.png"
+            name = self.image_name + "_fitting.png" 
             img.save(str(self.log_dir / name))
         return psnr, ms_ssim_value
-
 
 def image_path_to_tensor(image_path: Path):
     img = Image.open(image_path)
     transform = transforms.ToTensor()
-    img_tensor = transform(img).unsqueeze(0)  # [1, C, H, W]
+    img_tensor = transform(img).unsqueeze(0) #[1, C, H, W]
     return img_tensor
-
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(description="Example training script.")
@@ -137,8 +148,7 @@ def parse_args(argv):
         "--iterations", type=int, default=50000, help="number of training epochs (default: %(default)s)"
     )
     parser.add_argument(
-        "--model_name", type=str, default="GaussianImage_Cholesky",
-        help="model selection: GaussianImage_Cholesky, GaussianImage_RS, 3DGS"
+        "--model_name", type=str, default="GaussianImage_Cholesky", help="model selection: GaussianImage_Cholesky, GaussianImage_RS, 3DGS"
     )
     parser.add_argument(
         "--sh_degree", type=int, default=3, help="SH degree (default: %(default)s)"
@@ -161,7 +171,6 @@ def parse_args(argv):
     args = parser.parse_args(argv)
     return args
 
-
 def main(argv):
     args = parse_args(argv)
     # Cache the args as a text string to save them in the output dir later
@@ -177,43 +186,51 @@ def main(argv):
 
     logwriter = LogWriter(Path(f"./checkpoints/{args.data_name}/{args.model_name}_{args.iterations}_{args.num_points}"))
     psnrs, ms_ssims, training_times, eval_times, eval_fpses = [], [], [], [], []
-    image_h, image_w = 0, 0
-    if args.data_name == "kodak":
-        image_length, start = 24, 0
-    elif args.data_name == "DIV2K_valid_LRX2":
-        image_length, start = 100, 800
-    for i in range(start, start + image_length):
-        if args.data_name == "kodak":
-            image_path = Path(args.dataset) / f'kodim{i + 1:02}.png'
-        elif args.data_name == "DIV2K_valid_LRX2":
-            image_path = Path(args.dataset) / f'{i + 1:04}x2.png'
+    if args.data_name == "synthetic": # test-set
+        image_length, start = 200, 0
 
-        trainer = SimpleTrainer2d(image_path=image_path, num_points=args.num_points,
-                                  iterations=args.iterations, model_name=args.model_name, args=args,
-                                  model_path=args.model_path)
+    # warm-up: r_0 for 10k
+    image_path = Path(args.dataset) / 'r_0.png'# TODO: or. all imgs?
+    trainer = SimpleTrainer2d(image_path=image_path, num_points=args.num_points,
+                           iterations=10000, model_name=args.model_name, args=args,
+                           model_path=args.model_path)
+    psnr, ms_ssim, training_time, eval_time, eval_fps = trainer.train()
+    image_h, image_w = trainer.H, trainer.W
+    logwriter.write("Warmup: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, Training:{:.4f}s, Eval:{:.8f}s, FPS:{:.4f}".format(
+        image_h, image_w, psnr, ms_ssim, training_time, eval_time, eval_fps))
+
+    # deform-NN: r0 ~r199 for 50k
+    image_h, image_w = 800, 800
+    frames = list(range(image_length))
+    random.shuffle(frames)
+    frame_stack = frames.copy()
+    for iteration in range(0, args.iterations + 1):# 单图 -> 1 iter
+        # 所有图片，共用一个gaussian，deformable
+        if not frame_stack:
+            frame_stack = frames.copy()
+        idx = frame_stack.pop(randint(0, len(frame_stack)-1))# 随机弹出一张: r_idx
+        image_path = Path(args.dataset) / f'r_{idx}.png'
+        trainer = DefTrainer2d(image_path=image_path, num_points=args.num_points,
+            iterations=100, model_name=args.model_name, args=args, model_path=args.model_path)# 每张过100 iter 
         psnr, ms_ssim, training_time, eval_time, eval_fps = trainer.train()
         psnrs.append(psnr)
         ms_ssims.append(ms_ssim)
         training_times.append(training_time)
         eval_times.append(eval_time)
         eval_fpses.append(eval_fps)
-        image_h += trainer.H
-        image_w += trainer.W
         image_name = image_path.stem
         logwriter.write("{}: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, Training:{:.4f}s, Eval:{:.8f}s, FPS:{:.4f}".format(
             image_name, trainer.H, trainer.W, psnr, ms_ssim, training_time, eval_time, eval_fps))
-
+    # 在这里存ckpt：gaussian，deform
+    #################
     avg_psnr = torch.tensor(psnrs).mean().item()
     avg_ms_ssim = torch.tensor(ms_ssims).mean().item()
     avg_training_time = torch.tensor(training_times).mean().item()
     avg_eval_time = torch.tensor(eval_times).mean().item()
     avg_eval_fps = torch.tensor(eval_fpses).mean().item()
-    avg_h = image_h // image_length
-    avg_w = image_w // image_length
 
-    logwriter.write("Average: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, Training:{:.4f}s, Eval:{:.8f}s, FPS:{:.4f}".format(
-        avg_h, avg_w, avg_psnr, avg_ms_ssim, avg_training_time, avg_eval_time, avg_eval_fps))
-
+    logwriter.write("Frames: {}x{}, PSNR:{:.4f}, MS-SSIM:{:.4f}, Training:{:.4f}s, Eval:{:.8f}s, FPS:{:.4f}".format(
+        image_h, image_w, avg_psnr, avg_ms_ssim, avg_training_time, avg_eval_time, avg_eval_fps))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
